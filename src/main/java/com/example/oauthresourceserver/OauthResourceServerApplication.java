7d16cbc52592b1db63479485aaa770eb0ca74474
xzz1p <olezhka.kirilov.97@mail.ru>
2023-12-07 12:58:29 +0300

    Added new trusted classes

--- src/main/java/com/example/oauthresourceserver/OauthResourceServerApplication.java
+++ src/main/java/com/example/oauthresourceserver/OauthResourceServerApplication.java
@@ -1,8 +1,13 @@
 package com.example.oauthresourceserver;
 
+import com.project.core.commands.user.CreateUserProfileCommand;
+import com.project.core.queries.user.FetchJwkSet;
+import com.thoughtworks.xstream.XStream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import tokenlib.util.jwk.SimpleJWK;
 
 @SpringBootApplication
 @EnableWebSecurity
@@ -10,6 +15,22 @@ public class OauthResourceServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OauthResourceServerApplication.class, args);
+    }
+
+    @Bean
+    public XStream xStream() {
+        XStream xStream = new XStream();
+        registerClasses(xStream,
+                FetchJwkSet.class,
+                SimpleJWK.class);
+
+        return xStream;
+    }
+
+    private void registerClasses(XStream xStream, Class<?>... classes) {
+        for (Class<?> clazz : classes) {
+            xStream.allowTypeHierarchy(clazz);
+        }
     }
 
 }
