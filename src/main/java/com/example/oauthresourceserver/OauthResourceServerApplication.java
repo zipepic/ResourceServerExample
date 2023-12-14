40dcc78d3c2b7494535feb0c53d53bbda1dad52a
xzz1p <olezhka.kirilov.97@mail.ru>
2023-12-14 23:03:29 +0300

    Added classes in trusted packages

--- src/main/java/com/example/oauthresourceserver/OauthResourceServerApplication.java
+++ src/main/java/com/example/oauthresourceserver/OauthResourceServerApplication.java
@@ -1,6 +1,9 @@
 package com.example.oauthresourceserver;
 
+import com.project.core.dto.UserProfileDTO;
 import com.project.core.queries.FetchJwksQuery;
+import com.project.core.queries.user.FetchUserProfileDTOByUserIdQuery;
+import com.project.core.queries.user.FindUserIdByUserNameAndValidatePasswordQuery;
 import com.thoughtworks.xstream.XStream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -21,7 +24,10 @@ public class OauthResourceServerApplication {
         XStream xStream = new XStream();
         registerClasses(xStream,
                 SimpleJWK.class,
-                FetchJwksQuery.class);
+                FetchJwksQuery.class,
+                UserProfileDTO.class,
+                FetchUserProfileDTOByUserIdQuery.FetchUserProfileDTOByUserIdQueryBuilder.class,
+                FindUserIdByUserNameAndValidatePasswordQuery.class);
 
         return xStream;
     }
