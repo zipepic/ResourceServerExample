ab59491b488cf86506c849cef67ccfed049c4289
xzz1p <olezhka.kirilov.97@mail.ru>
2023-12-14 23:06:02 +0300

    Refactored

--- src/main/java/com/example/oauthresourceserver/controller/HomeController.java
+++ src/main/java/com/example/oauthresourceserver/controller/HomeController.java
@@ -1,5 +1,9 @@
 package com.example.oauthresourceserver.controller;
 
+import com.project.core.dto.UserProfileDTO;
+import com.project.core.queries.user.FetchUserProfileDTOByUserIdQuery;
+import org.axonframework.queryhandling.QueryGateway;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,13 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+    private final QueryGateway queryGateway;
+    @Autowired
+    public HomeController(QueryGateway queryGateway) {
+        this.queryGateway = queryGateway;
+    }
+
     @GetMapping("/")
     public String home() {
         return ("<h1>Welcome</h1>");
     }
     @GetMapping("/secured")
-    public String secured() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        return ("<h1>Secured</h1> " + authentication.getName());
+    public UserProfileDTO secured() {
+        var query = FetchUserProfileDTOByUserIdQuery.builder()
+                .userId(SecurityContextHolder.getContext().getAuthentication().getName())
+                .build();
+        var user = queryGateway.query(query, UserProfileDTO.class);
+        return user.join();
     }
 }
